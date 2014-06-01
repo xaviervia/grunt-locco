583597c2db8d303471e5a9561d5cb306242d69e2
Xavier Via <xavierviacanel@gmail.com>
2014-06-01 10:19:25 -0300

    Bumping locco version

--- tasks/locco.js
+++ tasks/locco.js
@@ -6,54 +6,46 @@
  * Licensed under the MIT license.
  */
 
-'use strict';
+'use strict'
 
-var locco = require("locco");
+var locco = require("locco")
 
 module.exports = function(grunt) {
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
-
-  grunt.registerMultiTask('locco', 'A Grunt plugin for creating locco documentation', function() {
+  // locco grunt task
+  // ----------------
+  //
+  // In your project's Gruntfile, add a section named `locco` to the data
+  // object passed into `grunt.initConfig()`.
+  //
+  // ```js
+  // grunt.initConfig({
+  //   locco: {
+  //     all: {
+  //       // Put here the patterns of files to be parsed for documentation
+  //       src: ["lib/**/*.js"]
+  //     },
+  //   },
+  // });
+  // ```
+  grunt.registerMultiTask(
+    'locco',
+    'A Grunt plugin for creating locco documentation',
+    function() {
 
     // Runs locco for each pattern
     this.data.src.forEach(function (pattern) {
-      grunt.log.writeln("Generating locco documentation for files matching " + pattern + " ...");
-      var files = locco(pattern);
-      grunt.log.writeln("Generated documentation for:");
+      grunt.log.writeln(
+        "Generating locco documentation for files matching " +
+        pattern + " ..." )
+
+      var files = locco(pattern)
+
+      grunt.log.writeln("Generated documentation for:")
       files.forEach(function (file) {
-        grunt.log.writeln("  " + file);
-      });
-    });
-/*
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
+        grunt.log.writeln("  " + file)
+      })
+    })
+  })
 
-      // Handle options.
-      src += options.punctuation;
-
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
-
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
-    });
-
-*/
-  });
-
-};
+}
